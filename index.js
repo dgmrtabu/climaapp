2347115adc37d9033ae8c4abdbec788ffd68f8d3
DAVID GABRIEL MONTAÑO ROCHA <david.montano@cns.gob.bo>
2021-04-28 08:53:16 -0400

    C-70 AppClima

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 const { leerInput, inquirerMenu, pausa } = require('./helpers/inquirer');
+const Busquedas = require('./models/busquedas');
 
 const main = async() => {
 
+
+    const busquedas = new Busquedas();
     let opt = '';
 
     do {
         opt = await inquirerMenu();
         console.log({ opt });
-        /*switch (opt) {
+        switch (opt) {
             case 1:
+                // Mostrar mensaje
+                const lugar = await leerInput('Ciudad a buscar:');
+                console.log(lugar);
+                // Buscar los lugares
+                // Seleccionar el lugar
+                // Clima
+                // Mostar resultados
+                console.log('\nInformacion de la ciudad\n');
+                console.log('Ciudad:');
+                console.log('Latitud:');
+                console.log('Longitud:');
+                console.log('Temperatura:');
+                console.log('Minima:');
+                console.log('Maxima:');
                 break;
             case 2:
                 break;
             case 0:
                 break;
 
-        }*/
+        }
 
-        await pausa();
+        if (opt !== 0) await pausa();
 
     } while (opt !== 0);
 };
---
+++ models/busquedas.js
@@ -0,0 +1,16 @@
+class Busquedas {
+
+    historial = ['Tegucigalpa', 'Madrid', 'San Jose'];
+
+    constructor() {
+        //TODO: Leer DB si existe
+    }
+
+    async ciudad(lugar = '') {
+        // Peticion http
+        console.log(lugar);
+    }
+
+}
+
+module.exports = Busquedas;
